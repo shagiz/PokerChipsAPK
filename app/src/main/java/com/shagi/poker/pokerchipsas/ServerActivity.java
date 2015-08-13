8a35a7eb41166867909ef6cd858cc8c9b61b722c
shagiz <javashagi@yandex.ru>
2015-08-13 19:02:00 +0300

    SPISOk

--- app/src/main/java/com/shagi/poker/pokerchipsas/ServerActivity.java
+++ app/src/main/java/com/shagi/poker/pokerchipsas/ServerActivity.java
@@ -2,14 +2,27 @@ package com.shagi.poker.pokerchipsas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Shagi on 10.08.2015.
  */
 public class ServerActivity extends Activity{
+    String[] data = {"1", "2", "3", "4", "5","6","7","8","9"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.server_activity);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, data);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        Spinner spinner=(Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("Title");
+
     }
+
 }
